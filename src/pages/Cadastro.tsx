2e35ab3aa0e9d5d4453789f94743208500f3c7c6
Pedro Lucas <pedrolucas1101@proton.me>
2024-10-17 05:48:01 -0300

    arrumar "escolaridade"

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import { User, Endereco } from '../types';
 import AddressModal from '../components/EnderecoModal';
-import { useNavigate } from 'react-router-dom';
 import { DatePicker, Input, Select, Button } from "antd";
 
 interface UserFormProps {
   onSubmit: (user: User) => void;
   initialData?: User;
 }
-
-
-
 
 const Cadastro: React.FC<UserFormProps> = ({ onSubmit, initialData }) => {
   const [formData, setFormData] = useState<User>(initialData || {
@@ -31,9 +27,8 @@ const Cadastro: React.FC<UserFormProps> = ({ onSubmit, initialData }) => {
   const handleEnderecoSubmit = (enderecoData: Endereco) => {
     setEndereco(enderecoData);
     setFormData({ ...formData, endereco: enderecoData });
-    setShowModal(false); // Fechar modal após adicionar o endereço
+    setShowModal(false);
   };
-
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -62,83 +57,79 @@ const Cadastro: React.FC<UserFormProps> = ({ onSubmit, initialData }) => {
   };
 
   return (
-<div className="container mx-auto size- p-8">
-  <h1 className="text-4xl font-bold mb-8">Serviço Social </h1>
-    <div className='size-full'>
-      <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-6 text-lg">
-        <Input
-          name="nome"
-          placeholder="Nome"
-          value={formData.nome}
-          onChange={handleChange}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <Input
-          name="telefone"
-          placeholder="Telefone"
-          value={formData.cpf}
-          onChange={handleChange}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <Input
-          name="rg"
-          placeholder="RG"
-          value={formData.rg}
-          onChange={handleChange}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <DatePicker
-          name="data"
-          placeholder="Data de nascimento"
-          type = "date"
-          onChange={handleDate}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <Input
-          name="cpf"
-          placeholder="CPF"
-          value={formData.telefone}
-          onChange={handleChange}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <Input
-          name="profissao"
-          placeholder="Profissão"
-          value={formData.profissao}
-          onChange={handleChange}
-          className="border rounded p-4 w-full text-lg"
-        />
-        <Select
-          placeholder="Escolaridade"
-          onChange={handleEscolaridade}
-          className='size-full text-lg'
-          options={[
-            { value: 'Ensino fundamental incompleto', label: 'Ensino fundamental incompleto' },
-            { value: 'Ensino fundamental completo', label: 'Ensino fundamental completo' },
-            { value: 'Ensino medio incompleto', label: 'Ensino médio incompleto' },
-            { value: 'Ensino médio completo', label: 'Ensino médio completo' },
-            { value: 'Ensino superior incompleto', label: 'Ensino superior incompleto' },
-            { value: 'Ensino superior completo', label: 'Ensino superior completo' },
-        ]}
-        />
-          {/* Botão para abrir o modal de adicionar endereço */}
+    <div className="container mx-auto p-4 md:p-8">
+      <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8 text-center">Serviço Social</h1>
+      <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md">
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <Input
+            name="nome"
+            placeholder="Nome"
+            value={formData.nome}
+            onChange={handleChange}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <Input
+            name="telefone"
+            placeholder="Telefone"
+            value={formData.telefone}
+            onChange={handleChange}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <Input
+            name="rg"
+            placeholder="RG"
+            value={formData.rg}
+            onChange={handleChange}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <DatePicker
+            name="data"
+            placeholder="Data de nascimento"
+            onChange={handleDate}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <Input
+            name="cpf"
+            placeholder="CPF"
+            value={formData.cpf}
+            onChange={handleChange}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <Input
+            name="profissao"
+            placeholder="Profissão"
+            value={formData.profissao}
+            onChange={handleChange}
+            className="border rounded p-2 md:p-4 w-full text-lg"
+          />
+          <Select
+            placeholder="Escolaridade"
+            onChange={handleEscolaridade}
+            className="w-full size-full text-lg"
+            options={[
+              { value: 'Ensino fundamental incompleto', label: 'Ensino fundamental incompleto' },
+              { value: 'Ensino fundamental completo', label: 'Ensino fundamental completo' },
+              { value: 'Ensino médio incompleto', label: 'Ensino médio incompleto' },
+              { value: 'Ensino médio completo', label: 'Ensino médio completo' },
+              { value: 'Ensino superior incompleto', label: 'Ensino superior incompleto' },
+              { value: 'Ensino superior completo', label: 'Ensino superior completo' },
+            ]}
+          />
           <button
             type="button"
             onClick={() => setShowModal(true)}
-            className=" bg-green-500 text-white p-4 w-full text-lg rounded"
+            className="md:col-span-2 bg-green-500 text-white p-2 md:p-4 w-full text-lg rounded"
           >
             Adicionar Endereço
           </button>
-
           <button
             type="submit"
-            className="col-span-2 bg-blue-500 text-white p-4 text-lg rounded w-full"
+            className="md:col-span-2 bg-blue-500 text-white p-2 md:p-4 w-full text-lg rounded"
           >
             Submit
           </button>
         </form>
 
-        {/* Renderizar o modal quando showModal for true */}
         {showModal && (
           <AddressModal
             onClose={() => setShowModal(false)}
@@ -147,7 +138,7 @@ const Cadastro: React.FC<UserFormProps> = ({ onSubmit, initialData }) => {
           />
         )}
       </div>
-</div>
+    </div>
   );
 };
 
